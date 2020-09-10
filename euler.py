4d6d8690a5839242e534404cd9c5811823b16e02
Luis090777 <57786749+Luis090777@users.noreply.github.com>
2020-09-09 21:28:53 -0600

    Implementación de la función e_cuadrática y e_lineal.
    Se uso una función auxiliar para calcular el factorial de un número

--- euler.py
+++ euler.py
@@ -1,8 +1,25 @@
-def e_cuadratica(n):
-    # Implemente esta función
-    return 0
 
+def factorial (n):
+    n_factorial = 1
+    while(n > 0):
+        n_factorial = n_factorial*n
+        n = n-1
+    return n_factorial 
+
+def e_cuadratica(n):
+    i = 0
+    r = 0
+    while(i<=n):
+        r += (1/factorial(i))
+        i += 1
+    return r
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+    i = 0
+    r = 0
+    factorial = 1
+    while(i<=n):
+        r += (1/factorial)
+        i += 1
+        factorial = factorial*i
+    return r
